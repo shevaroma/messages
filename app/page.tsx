fcb6f70e2d83085aaa5337779ef55bab824f4e3e
Roman Shevtsiv <>
2024-10-29 12:16:29 +0100

    Added SignIn component.

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import firebase from "@/app/firebase";
 import { signInWithPopup, signOut } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Button } from "@/components/ui/button";
+import SignIn from "@/components/sign-in";
 
 export default function Home() {
   const [user] = useAuthState(firebase.auth);
@@ -19,12 +20,10 @@ export default function Home() {
       </Button>
     </div>
   ) : (
-    <Button
-      onClick={() => {
+    <SignIn
+      signIn={() => {
         void signInWithPopup(firebase.auth, firebase.authProvider);
       }}
-    >
-      Sign in
-    </Button>
+    />
   );
 }
---
+++ components/sign-in.tsx
@@ -0,0 +1,30 @@
+import GoogleIcon from "@/components/ui/google-icon";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const SignIn = ({ signIn }: { signIn: () => void }) => (
+  <div className="flex h-screen w-full items-center justify-center px-4">
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">Sign In</CardTitle>
+        <CardDescription>
+          Join the conversation, stay in touch, and sync effortlessly with our
+          messaging app.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Button className="w-full" onClick={signIn}>
+          <GoogleIcon />
+          Sign in with Google
+        </Button>
+      </CardContent>
+    </Card>
+  </div>
+);
+export default SignIn;
